636c63429ac59fba462635794ed43de8540469b2
Yogesh Dhakane <156941796+yogeshdhakane001@users.noreply.github.com>
2024-04-18 23:17:11 +0530

    DAY 1 🚀 Swap 2 Number Without use Third Variable
    take the input user 2 number and Swap Number 
    a = a + b;
    b = a - b;
    c = a - b;

---
+++ swapnumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class swapnumber
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second Number");
+        int b = sc.nextInt();
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
